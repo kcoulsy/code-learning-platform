0db9953a58f9e9bf04fa9530404ad0bf0d459263
Kristian <kristiancoulson@gmail.com>
2026-02-04 08:16:06 +0000

    Fix SSR build

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,27 @@ const config = defineConfig({
     netlify(),
     viteReact(),
   ],
+  ssr: {
+    external: [
+      '@tanstack/react-query',
+      '@radix-ui/react-accordion',
+      '@radix-ui/react-checkbox',
+      '@radix-ui/react-collection',
+      '@radix-ui/react-collapsible',
+      '@radix-ui/react-dialog',
+      '@radix-ui/react-dismissable-layer',
+      '@radix-ui/react-focus-guards',
+      '@radix-ui/react-focus-scope',
+      '@radix-ui/react-label',
+      '@radix-ui/react-popper',
+      '@radix-ui/react-portal',
+      '@radix-ui/react-presence',
+      '@radix-ui/react-progress',
+      '@radix-ui/react-select',
+      '@radix-ui/react-tooltip',
+      'react-resizable-panels',
+    ],
+  },
 })
 
 export default config
